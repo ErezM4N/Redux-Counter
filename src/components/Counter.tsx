b192bbd646dacbdd8919d393ba82519520926d6e
ErezM4N <erezm4n@gmail.com>
2022-02-23 15:25:35 +0200

    redux toolkit

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,31 +1,57 @@
-import { Component } from 'react';
-
+import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+
+import { counterActions } from '../store/index';
 
 import classes from './Counter.module.scss';
 
+
+//import { INCREMENT } from '../store/index';
+
 const Counter = () => {
   const dispatch = useDispatch();
+  const [increment, setIncrement] = useState(1);
   const counter = useSelector((state: any) => state.counter);
+  const show = useSelector((state: any) => state.showCounter);
+
 
 
   const incrementHandler = () => {
-    dispatch({ type: 'increment' })
+    //dispatch({ type: INCREMENT })
+    dispatch(counterActions.increment());
   };
 
   const decrementHandler = () => {
-    dispatch({ type: 'decrement' })
+    dispatch(counterActions.decrement())
+  };
+  const increaseHandler = () => {
+    //dispatch({ type: 'increase', amount: increment })
+    dispatch(counterActions.increase(increment))
+   
+  };
+  // const increaseHandler = (val: number) => {
+  //   dispatch({ type: 'increase', amount: val })
+  // };
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter())
   };
 
-  const toggleCounterHandler = () => { };
+  const updateInputValue = (evt: any) => {
+    setIncrement(Number(evt.target.value));
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={decrementHandler}>Decrement</button>
+        <div>
+          <button onClick={increaseHandler}>Increase by .. </button>
+          <input onChange={evt => updateInputValue(evt)} type="number" name="" id="" />
+        </div>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,22 +1,78 @@
-import { createStore } from 'redux'
+import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const counterReducer = (state = { counter: 0 }, action: any) => {
-    
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + 1
-        }
+export const INCREMENT = 'increment';
+
+const initialState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment(state) { // allow to mutate the state cuz. its automacally do it for us
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state, action: any) {
+            state.counter = state.counter + action.payload; //<--action.amount;
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
+        },
     }
+});
 
-    if (action.type === 'decrement') {
-        return {
-            counter: state.counter - 1
-        }
-    }
 
-    return state;
-};
 
-const store = createStore(counterReducer);
+const store = configureStore({
+    reducer: counterSlice.reducer //{counter: counterSlice.reducer}
+});
+
+export const counterActions = counterSlice.actions;
 
 export default store;
+
+
+
+// const counterReducer = (state = initialState, action: any) => {
+
+
+//     switch (action.type) {
+//         //NEVER MUTATE THE STATE JUST OVVERRIDE IT AND RETURN NEW OBJECT
+//         case INCREMENT:
+//             //state.counter++
+//             return {
+//                 //ref val its easy accidentally override and change existing state
+//                 //obj and array are reference value
+//                 counter: state.counter + 1,
+//                 showCounter: state.showCounter
+//             };
+
+//         case 'increase':
+//             return {
+//                 counter: state.counter + action.amount,
+//                 showCounter: state.showCounter
+//             };
+
+//         case 'decrement':
+//             return {
+//                 counter: state.counter - 1,
+//                 showCounter: state.showCounter
+//             };
+
+//         case 'toggle':
+//             return {
+//                 counter: state.counter,
+//                 showCounter: !state.showCounter
+//             };
+
+//         default:
+//             return state;
+//     }
+
+
+// };
+
+//createStore --> configureStore
